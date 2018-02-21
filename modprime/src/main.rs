64f47a5e7443d9670724dc8cfecc80764065852a
snocl <snorredc@gmail.com>
2018-02-21 14:45:01 +0100

    Implement addition

--- modprime/src/main.rs
+++ modprime/src/main.rs
@@ -15,13 +15,19 @@ pub fn multiply_add(a: [u32; 3], b: [u32; 3], x: [u32; 2]) -> [u32; 5] {
     let c2 = (c1 >> 32) + a2x0;
     let c3 = c2 >> 32;
 
-    let r0 = c0 & 0xffffffff;
-    let r1 = (r0 >> 32) + (c1 & 0xffffffff) + a0x1;
-    let r2 = (r1 >> 32) + (c2 & 0xffffffff) + a1x1;
-    let r3 = (r2 >> 32) + (c3 & 0xffffffff) + a2x1;
-    let r4 = r3 >> 32;
+    let d0 = c0 & 0xffffffff;
+    let d1 = (d0 >> 32) + (c1 & 0xffffffff) + a0x1;
+    let d2 = (d1 >> 32) + (c2 & 0xffffffff) + a1x1;
+    let d3 = (d2 >> 32) + (c3 & 0xffffffff) + a2x1;
+    let d4 = d3 >> 32;
 
-    [r0 as u32, r1 as u32, r2 as u32, r3 as u32, r4 as u32]
+    let e0 = (d0 & 0xffffffff) + (b[0] as u64);
+    let e1 = (e0 >> 32) + (d1 & 0xffffffff) + (b[1] as u64);
+    let e2 = (e1 >> 32) + (d2 & 0xffffffff) + (b[2] as u64);
+    let e3 = (e2 >> 32) + (d3 & 0xffffffff);
+    let e4 = (e3 >> 32) + (d4 & 0xffffffff); // TODO: Not strictly necessary?
+
+    [e0 as u32, e1 as u32, e2 as u32, e3 as u32, e4 as u32]
 }
 
 #[test]
@@ -40,6 +46,24 @@ fn test_multiply_big() {
 fn test_multiply_max() {
     let r = multiply_add([0xffffffff, 0xffffffff, 0xffffffff], [0, 0, 0], [0xffffffff, 0xffffffff]);
     assert_eq!([1, 0, 4294967295, 4294967294, 4294967295], r);
+}
+
+#[test]
+fn test_multiply_add_small() {
+    let r = multiply_add([1, 0, 0], [3, 0, 0], [5, 0]);
+    assert_eq!([8, 0, 0, 0, 0], r);
+}
+
+#[test]
+fn test_multiply_add_big() {
+    let r = multiply_add([0x77777777, 0xdddddddd, 0x22222222], [0x55555555, 0xcccccccc, 0xffffffff], [0xeeeeeeee, 0x33333333]);
+    assert_eq!([706283511, 2576980377, 1450744507, 2023406816, 114532461], r);
+}
+
+#[test]
+fn test_multiply_add_max() {
+    let r = multiply_add([0xffffffff, 0xffffffff, 0xffffffff], [0xffffffff, 0xffffffff, 0xffffffff], [0xffffffff, 0xffffffff]);
+    assert_eq!([0, 0, 0xffffffff, 0xffffffff, 0xffffffff], r);
 }
 
 fn main() {
