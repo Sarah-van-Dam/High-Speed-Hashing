f761087bd28a2c646723a68519c6d11d22a8367b
snocl <snorredc@gmail.com>
2018-04-25 15:04:29 +0200

    Switch to u32 intermediaries for poly64

--- modprime/src/string.rs
+++ modprime/src/string.rs
@@ -46,38 +46,38 @@ pub fn poly16(a: u32, b: u32, c: u32, x: &[u16]) -> u32 {
     (h as u32) & 0xfffff
 }
 
-const P64: [u64; 3] = [0xffffffff, 0xffffffff, 0x01ffffff];
+const P64: [u32; 3] = [0xffffffff, 0xffffffff, 0x01ffffff];
 
 #[inline]
-pub fn op3_mul1(a: [u64; 3], x: u64) -> [u64; 4] {
-    let b0 = a[0] * x;
-    let b1 = a[1] * x;
-    let b2 = a[2] * x;
+pub fn op3_mul1(a: [u32; 3], x: u32) -> [u32; 4] {
+    let b0 = (a[0] as u64) * (x as u64);
+    let b1 = (a[1] as u64) * (x as u64);
+    let b2 = (a[2] as u64) * (x as u64);
 
     let c0 = b0;
     let c1 = (c0 >> 32) + b1;
     let c2 = (c1 >> 32) + b2;
     let c3 = c2 >> 32;
 
-    [c0 & 0xffffffff, c1 & 0xffffffff, c2 & 0xffffffff, c3 & 0xffffffff]
+    [c0 as u32, c1 as u32, c2 as u32, c3 as u32]
 }
 
 #[inline]
-pub fn op3_mul1_add3(a: [u64; 3], x: u64, b: [u64; 3]) -> [u64; 4] {
-    let c0 = a[0] * x + b[0];
-    let c1 = a[1] * x + b[1];
-    let c2 = a[2] * x + b[2];
+pub fn op3_mul1_add3(a: [u32; 3], x: u32, b: [u32; 3]) -> [u32; 4] {
+    let c0 = (a[0] as u64) * (x as u64) + (b[0] as u64);
+    let c1 = (a[1] as u64) * (x as u64) + (b[1] as u64);
+    let c2 = (a[2] as u64) * (x as u64) + (b[2] as u64);
 
     let d0 = c0;
     let d1 = (d0 >> 32) + c1;
     let d2 = (d1 >> 32) + c2;
     let d3 = d2 >> 32;
 
-    [d0 & 0xffffffff, d1 & 0xffffffff, d2 & 0xffffffff, d3 & 0xffffffff]
+    [d0 as u32, d1 as u32, d2 as u32, d3 as u32]
 }
 
 #[inline]
-pub fn op3_mul3(a: [u64; 3], x: [u64; 3]) -> [u64; 6] {
+pub fn op3_mul3(a: [u32; 3], x: [u32; 3]) -> [u32; 6] {
     let [r0, b1, b2, b3] = op3_mul1(a, x[0]);
     let [r1, c2, c3, c4] = op3_mul1_add3(a, x[1], [b1, b2, b3]);
     let [r2, r3, r4, r5] = op3_mul1_add3(a, x[2], [c2, c3, c4]);
@@ -85,24 +85,24 @@ pub fn op3_mul3(a: [u64; 3], x: [u64; 3]) -> [u64; 6] {
 }
 
 #[inline]
-pub fn op2p_digitsum_add1p(a: [u64; 6], b: [u64; 3]) -> [u64; 3] {
+pub fn op2p_digitsum_add1p(a: [u32; 6], b: [u32; 3]) -> [u32; 3] {
     let c0 = a[0];
     let c1 = a[1];
     let c2 = a[2] & 0x01ffffff;
 
-    let d0 = (a[2] >> 25) | ((a[3] << 7) & 0xffffffff);
-    let d1 = (a[3] >> 25) | ((a[4] << 7) & 0xffffffff);
-    let d2 = (a[4] >> 25) | ((a[5] << 7) & 0xffffffff);
+    let d0 = (a[2] >> 25) | (a[3] << 7);
+    let d1 = (a[3] >> 25) | (a[4] << 7);
+    let d2 = (a[4] >> 25) | (a[5] << 7);
 
-    let e0 = c0 + d0 + b[0];
-    let e1 = (e0 >> 32) + c1 + d1 + b[1];
-    let e2 = (e1 >> 32) + c2 + d2 + b[2];
+    let e0 = (c0 as u64) + (d0 as u64) + (b[0] as u64);
+    let e1 = (e0 >> 32) + (c1 as u64) + (d1 as u64) + (b[1] as u64);
+    let e2 = (e1 >> 32) + (c2 as u64) + (d2 as u64) + (b[2] as u64);
 
-    [e0 & 0xffffffff, e1 & 0xffffffff, e2 & 0xffffffff]
+    [e0 as u32, e1 as u32, e2 as u32]
 }
 
 #[inline]
-pub fn op1p_trysub1p(a: [u64; 3], b: [u64; 3]) -> [u64; 3] {
+pub fn op1p_trysub1p(a: [u32; 3], b: [u32; 3]) -> [u32; 3] {
     let c0 = (a[0] as i64) - (b[0] as i64);
     let c1 = (a[1] as i64) - (b[1] as i64);
     let c2 = (a[2] as i64) - (b[2] as i64);
@@ -114,23 +114,19 @@ pub fn op1p_trysub1p(a: [u64; 3], b: [u64; 3]) -> [u64; 3] {
     if d2 < 0 {
         a
     } else {
-        [(d0 as u64) & 0xffffffff, (d1 as u64) & 0xffffffff, (d2 as u64) & 0xffffffff]
+        [d0 as u32, d1 as u32, d2 as u32]
     }
 }
 
 pub fn poly64(a: [u32; 3], b: [u32; 3], c: [u32; 3], x: &[u64]) -> u32 {
-    let a = [a[0] as u64, a[1] as u64, a[2] as u64];
-    let b = [b[0] as u64, b[1] as u64, b[2] as u64];
-    let c = [c[0] as u64, c[1] as u64, c[2] as u64];
-
     if x.is_empty() {
-        return (b[0] & 0xfffff) as u32;
+        return b[0] & 0xfffff;
     }
 
-    let mut h = [x[0] & 0xffffffff, x[0] >> 32, 0];
+    let mut h = [x[0] as u32, (x[0] >> 32) as u32, 0];
     for &v in &x[1..] {
-        let v0 = v & 0xffffffff;
-        let v1 = v >> 32;
+        let v0 = v as u32;
+        let v1 = (v >> 32) as u32;
         let d = op2p_digitsum_add1p(op3_mul3(c, h), [v0, v1, 0]);
         h = op1p_trysub1p(op1p_trysub1p(d, P64), P64);
     }
